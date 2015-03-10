cd5ef13515e14ff44b5dfb7585125538898a08f0
matsuosh <shooohei.m@gmail.com>
2015-03-10 22:55:23 +0900

    工事中

--- TUBIN/View Controllers/YouTubePlayerViewController.swift
+++ TUBIN/View Controllers/YouTubePlayerViewController.swift
@@ -43,8 +43,10 @@ class YouTubePlayerViewController: UIViewController {
     override func viewWillAppear(animated: Bool) {
         navigationController?.setNavigationBarHidden(false, animated: true)
         configure(navigationItem: navigationItem)
+        // 再生中 -> 再生 OK
+        // 停止 -> 再生 ???
+        scrubberView.sync(player.controller)
         if player.isPlaying() {
-            scrubberView.sync(player.controller)
             addPlayerView(player.controller)
         }
         super.viewWillAppear(animated)
